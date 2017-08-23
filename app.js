ac8fb2efdb038c692934daa0ba9d950bc3c3481e
Keith Canty <keith.curtis.canty@gmail.com>
2017-08-23 16:41:07 -0400

    second commit, functionality is correct at this point

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-const express = require ('express')
-const mustache = require ('mustache-express')
-// const User = require ('./login/login.js')
-const app = express()
+const express = require ('express');
+const bodyParser = require('body-parser');
+const mustache = require ('mustache-express');
+const userData = require ('./data.js');
+const session = require('express-session');
+const app = express();
 
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 //set up engine view
-app.engine('mustache',mustache())
-app.set('view engine','mustache')
-app.set('views','/.views')
+app.engine('mustache',mustache());
+app.set('view engine','mustache');
+app.set('login','/.login');
 
 app.get('./admin',function(req,res){
 	//validation code to see if user is admin
 
+	res.render('index');
+
+});
+
+function verification(req, username, password){
+	var verifyUser = userData.users.find(function (user){
+	  if (username === user.username && password === user.password) {
+		req.session.verified = true;
+		console.log('Username and password credentials approved.');
+	} else {
+		return false
+	}
+});
+console.log(req.session);
+return req.session;
+}
+
+app.get('/', function (req,res){
+	res.render('index');
+});
+
+app.get('/login',function(req,res){
 	res.render('index')
+});
 
-})
+app.post('/',function(req,res){
+	console.log(req.body);
+	var username = req.body.username;
+	var password = req.body.password;
+	verification(req, username, password);
+		if (req.session && req.session.verified){
+			res.send(username + ' thanks for logging on.');
+		} else {
+			res.redirect('/login');
+		}
+	});
 
-// app.get('/admin/user-list',function(req,res){
-// 	res.redner('index')
-// })
-
-// app.get('/',function(req,res){
-// 	// var user = User.find('landry')
-// 		// res.send(user)
-// 		res.send('Hi Keith!')
-// }
-
-app.get('/',function(req,res){
-	res.render('index-mustache.html')
-})
-
-
-app.get('/list',function(req,res){
-	res.send(User.all)
-})
 
 app.listen(3000, function (){
-	console.log('Example app listening on port 3000!')
-})
+	console.log('App is now running on port 3000!')
+});
---
+++ data.js
@@ -0,0 +1,20 @@
+module.exports = {
+  users: [
+    {
+      username: "mom",
+      password: "twins"
+    },
+    {
+      username: "kevin",
+      password: "gravy"
+    },
+    {
+      username: "simone",
+      password: "abc123"
+    },
+    {
+      username: "keith",
+      password: "omgwtf"
+    }
+  ]
+}
